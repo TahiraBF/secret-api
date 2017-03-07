1dcbb1b998ba4d43a986ba32fe2ed37afa7ffa9c
Tahira Farid <tahirafarid@hotmail.co.uk>
2017-03-07 16:49:15 +0100

    encrypted seed user passwords to fix login issue

--- bin/seeds.js
+++ bin/seeds.js
@@ -1,11 +1,18 @@
 const mongoose     = require('mongoose');
 const User         = require('../models/user');
 mongoose.connect('mongodb://localhost/secret-place');
+const bcrypt        = require("bcrypt");
+const bcryptSalt    = 10;
+
+
+
+var salt     = bcrypt.genSaltSync(bcryptSalt);
+
 
 
 const users = [
   { username      : "Loop@fruit.com",
-    password      : "123",
+    password      : bcrypt.hashSync("123", salt),
     name          : "Fruit",
     travellerType : "Nomad",
     description   : "I have been travelling from longer than I could walk.",
@@ -15,7 +22,7 @@ const users = [
 
   },
   { username      : "p@p.com",
-    password      : "123",
+    password      : bcrypt.hashSync("123", salt),
     name          : "Prissy",
     travellerType : "Lujo",
     description   : "I never go anywhere untattended by a hoard of helpers. But i can keep a secret",
@@ -25,7 +32,7 @@ const users = [
 
   },
   { username      : "dd@fruit.com",
-    password      : "123",
+    password      : bcrypt.hashSync("abc", salt),
     name          : "Dave",
     travellerType : "Budget",
     description   : "I travelled across asia with nothing more than $4 in my pocket and flip-flops.",
@@ -37,19 +44,12 @@ const users = [
 ];
 
 
-
-
-
-
-
-
-
-
 User.create(users, (err, docs)=> {
   if (err){
 		throw(err);}
 
   docs.forEach( (user)=>{
+
 		console.log(user.username);
   });
   mongoose.connection.close();
