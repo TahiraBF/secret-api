5d640df6a0d9cac5516da51ce536c194a62a9bf6
Tahira Farid <tahirafarid@hotmail.co.uk>
2017-03-08 12:26:02 +0100

    profile route added to

--- routes/profile.js
+++ routes/profile.js
@@ -6,32 +6,56 @@ const Secret      = require("../models/secret");
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  // User.find({}, (err, users) => {
-  //   if (err) {
-  //     res.send(err);
-  //   } else {
-      PendingUser.find({}, (err, pendingUsers)=> {
-        if (err) {
-          res.send(err);
-        } else {
-              res.json({pendingUsers: pendingUsers});
-            }
-          });
-      // }
-    // });
+  PendingUser.find({}, (err, pendingUsers)=> {
+    if (err) {
+      res.send(err);
+    } else {
+      res.json(pendingUsers);
+    }
   });
+});
 
-// router.get('/', function(req, res, next) {
-//   var userId = req.params.id;
-//   User.findById(userId, (err, user) => {
-//     if (err) {
-//       res.send(err);
-//     } else {
-//       res.json(user);
-//     }
+router.get('/:id', (req, res, next) => {
+  var userId = req.params.id;
+
+  PendingUser.findById(userId, (err, user) => {
+    if (err){
+      res.send(err);
+    } else {
+      res.json(user);
+    }
+  });
+});
+
+
+
+router.delete('/', function(req, res, next) {
+
+});
+
+// router.post('/', function(req, res, next) {
+//
+//
+//
+//   const newUser = User({
+//     username,
+//     password,
+//     name,
+//     travellerType,
+//     description,
+//     refer : ""
+//     isDisclaimer: true,
+//     role: 'User'
 //   });
+//
+//   newUser.save((err, user) => {
+//     if (err) {
+//       res.status(400).json({ message: err });
+//     } else {
+//       res.status(200).json({message: "user saved"});
+//     }
+//   })
 // });
-
 
 
 module.exports = router;
