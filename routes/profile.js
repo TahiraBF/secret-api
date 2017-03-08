da663e97a36107bf8f36ca43c87b7342177d4398
Tahira Farid <tahirafarid@hotmail.co.uk>
2017-03-08 13:10:50 +0100

    profile.js delete method added and works

--- routes/profile.js
+++ routes/profile.js
@@ -3,6 +3,7 @@ var router        = express.Router();
 const User        = require("../models/user");
 const PendingUser = require("../models/pendingUser");
 const Secret      = require("../models/secret");
+const mongoose    = require('mongoose');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -27,35 +28,51 @@ router.get('/:id', (req, res, next) => {
   });
 });
 
-
-
 router.delete('/', function(req, res, next) {
 
 });
 
-// router.post('/', function(req, res, next) {
-//
-//
-//
-//   const newUser = User({
-//     username,
-//     password,
-//     name,
-//     travellerType,
-//     description,
-//     refer : ""
-//     isDisclaimer: true,
-//     role: 'User'
-//   });
-//
-//   newUser.save((err, user) => {
-//     if (err) {
-//       res.status(400).json({ message: err });
-//     } else {
-//       res.status(200).json({message: "user saved"});
-//     }
-//   })
-// });
+router.post('/:id', function(req, res, next) {
+  PendingUser.findById(req.params.id, (err, user) => {
+    if (err){
+      res.send(err);
+    } else {
+        const newUser = User({
+          username : user.username,
+          password : user.password,
+          name : user.name,
+          travellerType : user.travellerType,
+          description : user.description,
+          refer : " ",
+          isDisclaimer: true,
+          role: 'User'
+        });
+        newUser.save((err, user) => {
+          if (err) {
+            res.status(400).json({ message: err });
+          } else {
+            res.status(200).json({message: "user saved"});
+          }
+      });
+    }
+  });
+});
+
+router.delete('/:id', (req, res) => {
+  if(!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    return res.status(400).json({ message: 'Specified id is not valid' });
+  }
+
+  PendingUser.remove({ _id: req.params.id }, (err) => {
+    if (err) {
+      return res.send(err);
+    } else {
+      res.json({
+        message: 'Pending user has been removed!'
+      });
+    }
+  });
+});
 
 
 module.exports = router;
